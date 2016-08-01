f7fdf4e8e70f6010a40b83f8bbca26e2e3cd8b53
Jamie <up733026@myport.ac.uk>
2016-08-01 10:09:51 +0100

    Heroku initialise

--- app/assets/javascripts/display_handler.js
+++ app/assets/javascripts/display_handler.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function(){
   $(".toggle_display").click(function(){
-    display_gantt();
+    //drawGantt();
     $("#create_tasks").toggle("slow");
     $("#display_new_task").toggle();
     $("#gantt_div").toggle("slow");
@@ -8,15 +8,6 @@ $(document).on('turbolinks:load', function(){
 
   });
 });
-
-function display_gantt(){
-  var gantt_display = $("#gantt_div").is(":hidden");
-  var html_rows = $(".task_display tr").length;
-  console.log(html_rows);
-  var rows = html_rows - 2;
-
-}
-
 
 //impliments drag and drop
 $(document).on('turbolinks:load', function(){
@@ -26,7 +17,7 @@ $(document).on('turbolinks:load', function(){
       update: function(event, ui){
 
         var item_id = ui.item.data('item-id');
-        console.log(item_id);
+        //console.log(item_id);
         var position = ui.item.index();
         //console.log(position);
         var project_id = $('#hidden_page_id')[0].innerHTML;
@@ -38,7 +29,19 @@ $(document).on('turbolinks:load', function(){
           data: { task: {task_id: item_id, task_number_position: position}}
         }
         )
+        drawGantt();
       }
 
     });
   });
+
+  function drawGantt(){
+    //console.log("Drawing Gantt");
+    var rows = $("#task_display tr").length;
+    //console.log(rows);
+    for(var i = 1; i < rows; i++){
+      var start_date = $('#task_display')[0];
+      console.log(start_date);
+    }
+
+  }
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   def create
     @project = Project.find(params[:project_id])
-    params[:task][:task_number] = @project.tasks.last.task_number + 1
+    #params[:task][:task_number] = @project.tasks.last.task_number + 1
     @task = @project.tasks.create(task_params)
     redirect_to project_path(@project)
   end
@@ -22,8 +22,8 @@ class TasksController < ApplicationController
       :name,
       :start_date,
       :finish_date,
-      :prerequisite,
-      :task_number
+      :prerequisite#,
+      #:task_number
     )
   end
 end
