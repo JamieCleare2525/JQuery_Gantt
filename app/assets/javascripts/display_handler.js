7745e88f3bae1f15a5a1e55ebaa8440acb135332
Jamie <up733026@myport.ac.uk>
2016-08-02 10:21:41 +0100

    Gantt Chart now draws with date increments

--- app/assets/javascripts/display_handler.js
+++ app/assets/javascripts/display_handler.js
@@ -45,14 +45,21 @@ function drawGantt(){
   while (svg.lastChild){
     svg.removeChild(svg.lastChild);
   }
+  var project_start = $('#task_display tr:has(td:contains("Start"))')[0];
+  var pro_start_cells = project_start.cells;
+  var pro_start_date = pro_start_cells[1].innerHTML;
+  console.log(pro_start_date);
   var rows = $("#task_display tr").length;
   //console.log(rows);
   for(var i = 1; i < rows; i++){
     var task_attr = document.getElementById("task_display").rows[i].cells;
+
     var start = task_attr[1].innerHTML;
     var finish = task_attr[2].innerHTML;
+    var dist_from_start = dateDifference(pro_start_date, start);
+    //console.log(dist_from_start);
     var barlength = dateDifference(start, finish);
-    drawBar(barlength, i);
+    drawBar(barlength, i, dist_from_start);
 
   }
 }
@@ -61,14 +68,14 @@ function dateDifference(early_date, later_date){
   var oneDay = 24*60*60*1000;
   start = new Date(early_date);
   finish = new Date(later_date);
-  console.log("Start: "+start+"Finish: "+finish);
+  //console.log("Start: "+start+"Finish: "+finish);
   var dateDiff =
   Math.round((finish.getTime() - start.getTime())/(oneDay));
-  console.log(dateDiff);
+  //console.log(dateDiff);
   return dateDiff;
 }
 
-function drawBar(dateDiff, row_number){
+function drawBar(dateDiff, row_number, dist_from_start){
   //changes the color of the bar depending on if the task is even or odd
   var color;
   if ((row_number % 2) == 0){
@@ -80,10 +87,10 @@ function drawBar(dateDiff, row_number){
   //creates the bars in the bar chart of the gantt
   //var dateInc = differenceFromStart(row_number);
   var svg_area = document.getElementById('gantt_display');
-  svg_area.style.height = (68 * row_number) + 'px';
+  svg_area.style.height = (68 * (row_number+1)) + 'px';
   var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
   rect.setAttributeNS(null, 'id', 'task_bar'+row_number);
-  rect.setAttributeNS(null, 'x', 0);//34 * dateInc);
+  rect.setAttributeNS(null, 'x', 10 * dist_from_start);
   rect.setAttributeNS(null, 'y', ((44 * (row_number))+22));
   rect.setAttributeNS(null, 'height', '45');
   rect.setAttributeNS(null, 'width', '10' * dateDiff);
@@ -92,7 +99,7 @@ function drawBar(dateDiff, row_number){
   //creates text to appear in the rect.
   var newText = document.createElementNS("http://www.w3.org/2000/svg", "text");
   newText.setAttributeNS(null, 'id', 'bar_text'+row_number);
-  newText.setAttributeNS(null, 'x', 20);//(34 * dateInc + (dateDiff*16)));
+  newText.setAttributeNS(null, 'x', 20 + (dist_from_start * 10));
   newText.setAttributeNS(null, 'y', ((44*(row_number-1))+80));
   newText.setAttributeNS(null, 'font-size', "10px");
   newText.setAttributeNS(null, 'text-anchor', "middle");
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :project
   include RankedModel
-  ranks :task_number
+  ranks :task_number,
+    :with_same => :project_id
   validates :name, presence: true
   validates :start_date, presence: true
 end
